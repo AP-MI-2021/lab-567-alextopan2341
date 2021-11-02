5fde9c3f50afb8ef68b02ddace27aaf4b5831a6a
Alex <alexandrutopanfarcau@gmail.com>
2021-11-02 08:44:41 +0200

    Lab 5-7 -> part 2(exercitiile 2.2, 2.3, 2.4, 2.5, 2.6)+ ex lab

--- UI/command_line_console.py
+++ UI/command_line_console.py
@@ -1,28 +1,44 @@
-from Logic.CRUD import adauga_obiect, stergere_obiect
+from Logic.CRUD import adauga_obiect, stergere_obiect, modificare_obiect
+
 
 def print_menu():
-    print("1. Adaugare obiect")
-    print("2. Stergere obiect")
-    print("a. Afisare toate obiectele")
-
+    print("add. Adaugare obiect")
+    print("delete. Stergere obiect")
+    print("change. Modificare obiect")
+    print("showall. Afisare toate obiectele")
+    print("exit")
 
 def command_line_console(lista):
+    lista = []
     while True:
         print_menu()
-        optiune = input("Dati optiunea: ")
-        if optiune == "add":
-            id = input("Dati id: ")
-            nume = input("Dati nume: ")
-            descriere = input("Dati descriere: ")
-            pret_achizitie = float(input("Dati pretul: "))
-            locatie = input("Dati locatie: ")
+        numere = []
+        string_citit = input("Dati optiunea: ")
+        numere = string_citit.split(",")
+        if numere[0] == "add":
+            id = numere[1]
+            nume = numere[2]
+            descriere = numere[3]
+            pret_achizitie = float(numere[4])
+            locatie = numere[5]
             lista = adauga_obiect(lista, id, nume, descriere, pret_achizitie, locatie)
-        elif optiune == "showall":
+            if numere[6] == "showall":
+                print(lista)
+        elif numere[0] == "change":
+            id = numere[1]
+            nume = numere[2]
+            descriere = numere[3]
+            pret_achizitie = float(numere[4])
+            locatie = numere[5]
+            lista = modificare_obiect(lista, id, nume, descriere, pret_achizitie, locatie)
+            if numere[6] == "showall":
+                print(lista)
+        elif numere[0] == "showall":
             print(lista)
-        elif optiune == "stop":
+        elif numere[0] == "exit":
             return 0
-        elif optiune == "delete":
-            id = input("Dati id-ul dorit: ")
+        elif numere[0] == "delete":
+            id = numere[1]
             lista = stergere_obiect(id, lista)
         else:
             print("Optiune gresita! Reincercati!")
